ab4780f19980e39b2dcedfc86c887d8b401944d0
Josephtellolaiza <142683026+Josephtellolaiza@users.noreply.github.com>
2023-10-28 11:44:46 -0500

    Add files via upload
    Adicion del proyecto

---
+++ ProgramaEstudiante2023/src/programaestudiante2023/TestPrincipal.java
@@ -0,0 +1,13 @@
+package programaestudiante2023;
+
+
+
+public class TestPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Estudiantes2023 xxx = new Estudiantes2023();
+		xxx.listarEstudiantes();
+	}
+
+}
